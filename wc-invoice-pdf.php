701947bdb8242dbe0415c765681b73eb18e03533
ole <ole.k@web.de>
2017-10-21 21:12:58 +0200

    added pdf-php library

---
+++ wc-invoice-pdf.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: WC Invoice PDF
+ * Description: WooCommerce invoice pdf plugin with recurring payments (scheduled)
+ * Version: 1.0.0
+ * Author: ole1986 <ole.k@web.de>
+ * Author URI: https://github.com/ole1986/wc-invoice-pdf
+ * Text Domain: wc-invoice-pdf
+ */
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
