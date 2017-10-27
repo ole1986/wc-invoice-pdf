bdefa2882d66c31939e52419bb115cdb7a30be68
ole <ole.k@web.de>
2017-10-27 08:51:55 +0200

    allow adding media images as company log in pdf

--- wc-invoice-pdf.php
+++ wc-invoice-pdf.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: WC Recurring Invoice PDF
  * Description: WooCommerce invoice pdf plugin with recurring payments (scheduled)
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: ole1986 <ole.k@web.de>
  * Author URI: https://github.com/ole1986/wc-invoice-pdf
  * Text Domain: wc-invoice-pdf
@@ -144,6 +144,8 @@ class WCInvoicePdf {
             if(!empty($args[$k])) $xargs[$k] = $args[$k];
         }
 
+        if($type == 'media') $xargs['container'] = 'div';
+
         echo '<' . $xargs['container'];
         foreach ($xargs['attr'] as $k => $v) {
             echo ' '.$k.'="'.$v.'"';
@@ -179,7 +181,19 @@ class WCInvoicePdf {
             echo '<div '.$attrStr.'>';
             wp_editor($optValue, $name, ['teeny' => true, 'editor_height'=>200, 'media_buttons' => false]);
             echo '</div>'; 
-        }           
+        } else if($type == 'media'){
+            wp_enqueue_media();
+            $url = '';
+            if(intval($optValue) > 0) {
+                $url = wp_get_attachment_url($optValue);
+            }
+            echo "<div class='image-preview-wrapper' style='display:inline-block;'>";
+            echo "<img id='${name}-preview' src=\"$url\" style='max-height: 100px;'><br />";
+            echo "<input onclick=\"WCInvoicePdfAdmin.OpenMedia(this,'$name')\" type=\"button\" class=\"button\" value=\"" . __( 'Select image' ) ."\" />";
+            echo "<input onclick=\"WCInvoicePdfAdmin.ClearMedia(this,'$name')\" type=\"button\" class=\"button\" value=\"" . __( 'Clear image' ) ."\" />";
+            echo "<input type='hidden' name=\"".$name."\" id='$name' value=\"$optValue\" />";
+            echo "</div>";
+        }
         echo '</' . $xargs['container'] .'>';
     }
 
