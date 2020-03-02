37b50bc77a5512d18401b2fbd2f246d1e687afff
ole <ole.k@web.de>
2020-03-02 13:55:00 +0100

    Version 1.5.5

--- wc-invoice-pdf.php
+++ wc-invoice-pdf.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: WC Recurring Invoice PDF
  * Description: WooCommerce invoice pdf plugin with recurring payments (scheduled)
- * Version: 1.5.4
+ * Version: 1.5.5
  * Author: ole1986 <ole.k@web.de>
  * Author URI: https://github.com/ole1986/wc-invoice-pdf
  * Plugin URI: https://github.com/ole1986/wc-invoice-pdf/releases
