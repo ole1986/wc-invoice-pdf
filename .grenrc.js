5ffb39c7d640c2fa32f028aab86252d998f82815
ole1986 <ole.koeckemann@gmail.com>
2021-03-10 20:06:44 +0100

    Fixed gren

--- .grenrc.js
+++ .grenrc.js
@@ -1,10 +1,18 @@
+
 function getAuthor(placeholders) {
     if (placeholders.author === 'ole1986') {
         // skip owner
         return '';
     }
-    return `- ${placeholders.author ? `@${placeholders.author}` : name}`;
+
+    if (placeholders.author === null) {
+        // skip when no author could be found
+        return '';
+    }
+
+    return `- @${placeholders.author}`;
 }
+
 function parseCommitLine(placeholders) {
     return `- ${placeholders.message} ${getAuthor(placeholders)}`
 }
