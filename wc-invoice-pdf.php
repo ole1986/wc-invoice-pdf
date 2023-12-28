0f3625a0e910e71688eafe5cc454663c42560a0d
ole1986 <ole.koeckemann@gmail.com>
2023-12-28 21:29:26 +0100

    updated compatibility to WC and wordpress

--- wc-invoice-pdf.php
+++ wc-invoice-pdf.php
@@ -2,14 +2,14 @@
 /*
  * Plugin Name: WC Recurring Invoice PDF
  * Description: WooCommerce invoice pdf plugin with recurring payments (scheduled)
- * Version: 1.5.18
+ * Version: 1.5.19
  * Author: ole1986 <ole.k@web.de>
  * Author URI: https://github.com/ole1986/wc-invoice-pdf
  * Plugin URI: https://github.com/ole1986/wc-invoice-pdf/releases
  * Text Domain: wc-invoice-pdf
  *
  * WC requires at least: 3.0.0
- * WC tested up to: 5.5
+ * WC tested up to: 8.4
  */
 
 namespace WCInvoicePdf;
