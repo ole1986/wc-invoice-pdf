e60abcce96af1e46413f14de4cc788af22af6ebd
ole1986 <ole.koeckemann@gmail.com>
2025-09-18 21:08:22 +0200

    Updated docker-compose and user woocommerce version 10.1

--- wp-install.sh
+++ wp-install.sh
@@ -5,7 +5,7 @@ wp core install --url=http://localhost --title="${WP_TITLE:=Demo}" --admin_user=
 # Remove unused plugins
 wp plugin uninstall hello akismet
 # Install necessary plugins
-wp plugin install woocommerce --version=9.3.3
+wp plugin install woocommerce --version=10.1.2
 # Install neccesary plugin
 wp plugin activate woocommerce
 wp plugin activate wc-recurring-pdf
